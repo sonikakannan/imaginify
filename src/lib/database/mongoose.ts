af5a675973160b3011f5c57c9082171e46f16ec2
sonikakannan <sonikakannan66@gmail.com>
2024-11-30 11:32:09 +0530

    first commit

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -7,29 +7,26 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-declare global {
-  // Ensure global.mongoose is typed
-  var mongoose: MongooseConnection | undefined;
+let cached: MongooseConnection = (global as any).mongoose
+
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-let cached: MongooseConnection = global.mongoose || { conn: null, promise: null };
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-export const connectToDatabase = async (): Promise<Mongoose> => {
-  if (cached.conn) return cached.conn;
-
-  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: 'imaginify',
-      bufferCommands: false,
-    });
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
+    })
 
   cached.conn = await cached.promise;
+
   return cached.conn;
-};
+}
